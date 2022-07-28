e218bda26d92683e8f4ed85bbab55c0e8558d391
iambalakrishnan <impythonprogrammer@gmail.com>
2022-07-28 20:14:28 +0530

    duplicate string element finder added

---
+++ Ransom_Note.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+class Solution(object):
+  def canSpell(self, magazine, note):
+    letters = defaultdict(int)
+    for c in magazine:
+      letters[c] += 1
+    #print(letters)
+    for c in note:
+      if letters[c] <= 0:
+        return False
+      letters[c] -= 1
+
+    return True
+
+print(Solution().canSpell(['a', 'b', 'c', 'd', 'e', 'f'], 'bed'))
+# True
+
+print(Solution().canSpell(['a', 'b', 'c', 'd', 'e', 'f'], 'cat'))
+# # False
+
+print(Solution().canSpell(['a', 'b', 'c', 'd', 'e', 'f','b'], 'bbed'))
+## True
---
+++ duplicate_StringElement_InList.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isDuplicate(self, mylist):
+        mylist = ["python","pandas","numpy","pandas"] # the original input list with repeated elements.
+        dup = {x for x in mylist if mylist.count(x) > 1}
+        dup = list(dup)
+        mylist = set(mylist)
+        for i in mylist:
+            if i in dup:
+                print(f"{i} is a duplicate")
+            else:
+                print(f"{i} is not a duplicate")
+        
+
+print(Solution().isDuplicate(["python","pandas","numpy","pandas"]))
